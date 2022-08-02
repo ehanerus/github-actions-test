06ddf7e1552aaff20e2de3b8058aaac6b9c14be7
Emeric Hanerus <>
2022-08-02 16:12:57 +0200

    channel secret fix, entrypoint.sh revert

--- .github/entrypoint.sh
+++ .github/entrypoint.sh
@@ -6,9 +6,6 @@ if test -z "$SLACK_BOT_TOKEN"; then
   exit 1
 fi
 
-#grep -rl matchstring somedir/ | xargs sed -i 's/string1/string2/g'
-grep -o '"github: "'
-
 curl -X POST \
      -H "Content-type: application/json; charset=utf-8" \
      -H "Authorization: Bearer $SLACK_BOT_TOKEN" \
